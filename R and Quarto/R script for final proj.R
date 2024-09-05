45241f12c24e5efdcda6a57bcf2b6835ab552075
Victoria Ngo <victoriagngo@gmail.com>
2024-09-04 21:41:51 -0400

    File name updated

---
+++ R and Quarto/R script for final proj.R
@@ -0,0 +1,57 @@
+#load packages
+library(tidyverse)
+library(gtsummary)
+library(here)
+
+#load in clean data
+ikea <- read.csv(here::here("Data", "ikea.csv"))
+view(ikea)
+glimpse(ikea)
+
+# gt summary table
+tbl_summary(
+	ikea,
+	by = sellable_online,
+	include = c(category, other_colors),
+label = list(
+	category ~"Furniture Category",
+	other_colors ~"Other Colors Sold"
+))
+
+# regression and results
+tbl_uvregression(
+	ikea,
+	y = price,
+	include = c(category, sellable_online, other_colors, height, depth, width),
+	method = lm,
+	label = list(
+		category ~"Furniture Category",
+		other_colors ~"Other Colors Sold",
+		sellable_online ~"Sellable Online",
+  	height ~"Height in cm",
+  	depth ~"Depth in cm",
+  	width ~"Width in cm"),
+) |>
+	bold_labels()
+
+#data figure: Basic histogram with custom titles and labels
+png(filename = here("Figure","ikeawidthdistr.png"))
+
+hist(ikea$width,
+		 main = "Distribution of IKEA Furniture Width",  # title of the histogram
+		 xlab = "Width (in cm)",                         # x-axis label
+		 ylab = "Frequency",                             # y-axis label
+		 col = "#1E90FF",                                # color of the bars
+		 border = "black")                               # color of the bar borders
+
+dev.off()
+
+#custom function for sd
+custom_sd <- function(x) {
+	n <- length(x)
+	mean_x <- mean(x, na.rm = TRUE)
+	sqrt(sum((x - mean_x)^2) / (n - 1))
+}
+
+# usage fo custom function:
+custom_sd(ikea$price)
